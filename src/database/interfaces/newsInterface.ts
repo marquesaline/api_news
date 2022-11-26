c2284c01d0ef7caec66e6a069e869bb5cebb3e21
marquesaline <alinem_oliveira@yahoo.com>
2022-11-26 13:50:21 -0300

    Criação do service e da interface

---
+++ src/database/interfaces/newsInterface.ts
@@ -0,0 +1,12 @@
+interface INews {
+    hat: String,
+    title: String,
+    text: String,
+    author: String,
+    img: String, 
+    publishDate: Date,
+    link: String, 
+    active: Boolean
+}
+
+export default INews;
