3b4febea3c34e2e5a10f7a7bc9376eadd45011bd
marquesaline <alinem_oliveira@yahoo.com>
2022-12-03 14:34:52 -0300

    configuração do dockerfile

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,5 @@
 import 'dotenv/config';
 
-export const DB_URL: string = process.env.DB_URL_ENV ?? '';
+export const DB_URL_LOCAL: string = process.env.DB_URL_LOCAL ?? '';
+
+export const DB_URL: string = process.env.DB_URL ?? '';
