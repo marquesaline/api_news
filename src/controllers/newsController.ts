ea913510c373fbcc217bb3a5ee450b407d7a9f30
marquesaline <alinem_oliveira@yahoo.com>
2022-12-02 21:30:30 -0300

    inclusão dos testes unitários

--- src/controllers/newsController.ts
+++ src/controllers/newsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import httpStatus from 'http-status';
+import { StatusCodes } from 'http-status-codes';
 import NewsServices from '../database/services/newsService.';
 import Helper from '../utils/helper';
 
@@ -11,24 +11,24 @@ class NewsController {
 
     async getAll(req: Request, res: Response) {
         NewsServices.getAll()
-            .then(news => Helper.sendResponse(res, httpStatus[200], news))
-            .catch(error => Helper.sendResponse(res, httpStatus[500], error));
+            .then(news => Helper.sendResponse(res, StatusCodes.OK, news))
+            .catch(error => Helper.sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, error));
 
     }
     async getById(req: Request, res: Response) {
         const _id = req.params.id;
 
         NewsServices.getById(_id)
-            .then(news => Helper.sendResponse(res, httpStatus[200], news))
-            .catch(error => Helper.sendResponse(res, httpStatus[500], error));
+            .then(news => Helper.sendResponse(res, StatusCodes.OK, news))
+            .catch(error => Helper.sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, error));
     }
 
     async create(req: Request, res: Response) {
         const newNews = req.body;
 
         NewsServices.create(newNews)
-            .then(news => Helper.sendResponse(res, httpStatus[201], news, 'Notícia cadastrada com sucesso!'))
-            .catch(error => Helper.sendResponse(res, httpStatus[500], error));
+            .then(news => Helper.sendResponse(res, StatusCodes.CREATED, news, 'Notícia cadastrada com sucesso!'))
+            .catch(error => Helper.sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, error));
     }
 
     async update(req: Request, res: Response) {
@@ -36,16 +36,16 @@ class NewsController {
         const _id = req.params.id;
 
         NewsServices.update(_id, newNews)
-            .then(news => Helper.sendResponse(res, httpStatus[202], news, 'Notícia atualizada com sucesso!'))
-            .catch(error => Helper.sendResponse(res, httpStatus[500], error));
+            .then(news => Helper.sendResponse(res, StatusCodes.ACCEPTED, news, 'Notícia atualizada com sucesso!'))
+            .catch(error => Helper.sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, error));
     }
 
     async delete(req: Request, res: Response) {
         const _id = req.params.id;
 
         NewsServices.delete(_id)
-            .then(() => Helper.sendResponse(res, httpStatus[200], 'Notícia excluída com sucesso!'))
-            .catch(error => Helper.sendResponse(res, httpStatus[500], error));
+            .then(() => Helper.sendResponse(res, StatusCodes.OK, 'Notícia excluída com sucesso!'))
+            .catch(error => Helper.sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, error));
     }
 
 }
